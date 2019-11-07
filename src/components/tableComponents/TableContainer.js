26049e5b8d5fd0d1ee33efa74898341cd86adb8c
drikalabs <andrika.bogal@gmail.com>
2019-11-07 16:49:22 +0700

    create form modal for table input

--- src/components/tableComponents/TableContainer.js
+++ src/components/tableComponents/TableContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
 import './TableCard.scss'
 import { fetchDataTable } from './tableService/TableService'
 import { connect } from 'react-redux'
@@ -26,8 +26,54 @@ class TableContainer extends Component {
         return (
             <div>
                 <IconButton aria-label="settings" className="addTable">
-                    <AddTable />
+                    <AddTable data-toggle="modal" data-target="#exampleModal"/>
                 </IconButton>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog " role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="form-group row">
+                                        <label htmlFor="inputNumber"
+                                               className="col-sm-4 col-form-label">Number Table</label>
+                                        <div className="col-sm-8">
+                                            <input type="text" className="form-control" id="inputNumber" required/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group row">
+                                        <label htmlFor="inputCapacity"
+                                               className="col-sm-4 col-form-label">Capacity</label>
+                                        <div className="col-sm-8">
+                                            <input type="number" className="form-control" id="inputCapacity" required/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group row">
+                                        <label htmlFor="selectStatus"
+                                               className="col-sm-4 col-form-label">Status</label>
+                                        <div className="col-sm-8">
+                                            <select name="selectStatus" id="selectStatus" class="custom-select custom-select-md mb-3">
+                                                <option value="null">....</option>
+                                                <option value="AVALIABLE">Avaliable</option>
+                                                <option value="DINING">Dining</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Grid container spacing={3}>
                     {
                         this.props.fetchResult.map((element, index) => {
